702191eaa9727c17459e24fd838d910822fc779b
rodrsil <106642298+rodrsil@users.noreply.github.com>
2024-05-10 19:04:24 -0300

    challenge resolution

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,32 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Level { BASIC, INTERMEDIATE, DIFFICULT }
 
-class Usuario
+class User(var name: String, var email: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class EducationalContent(var name: String, val duration: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formation(val name: String, var difficulty: Level, var contents: List<EducationalContent>) {
+    val subscribers = mutableListOf<User>()
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun enroll(user: User) {
+        subscribers.add(user)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val programmingLogic = EducationalContent("Lógica de Programação", 40)
+    val java = EducationalContent("Java", 80)
+    val kotlin = EducationalContent("Kotlin", 80)
+
+    val mobile = Formation("Formação Mobile", Level.BASIC, listOf(programmingLogic, java, kotlin))
+
+    val student1 = User("Rodrigo", "rodrigo@email.com")
+    val student2 = User("Rafael", "rafael@email.com")
+
+    mobile.enroll(student1)
+    mobile.enroll(student2)
+
+    println("Inscritos na formação Mobile:")
+    mobile.subscribers.forEach { println(it.name) }
 }
